3475783fbd624f6d457c6b347ce45262ae4d37a1
Fulya Akan <fulyaakan@outlook.com>
2026-04-23 19:59:07 +0300

    Number pressed.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstNumber: Double = 0 //işlemden önceki sayı
+    var secondNumber: Double = 0 //işlemden sonraki sayı
+    var currentAction: String = ""
+    var writingNumber: Bool = false //kullanıcı ikinci sayıyı yazmaya basladı 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        //take the number on button, sender yazdıgın butonu temsil eder
+        guard let number = sender.currentTitle else { return }
+        
+        //add numver to label yazıyı labele ekle
+        if resultLabel.text == "0" {
+            resultLabel.text = number
+        } else {
+            // + --> string connection
+            resultLabel.text = resultLabel.text! + number
+        }
 
+    }
+    
+    @IBAction func actionPressed(_ sender: UIButton) {
+    }
 }
 
