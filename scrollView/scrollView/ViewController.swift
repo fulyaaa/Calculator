139ac0fb847fe6334a405448c8ef444c826d1a48
Fulya Akan <fulyaakan@outlook.com>
2026-04-28 13:57:40 +0300

    .
    Finalize storyboard layout.

---
+++ scrollView/scrollView/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  scrollView
+//
+//  Created by fulya akan on 25.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UIView!
+    
+    
+
+}
+
