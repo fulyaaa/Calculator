b6a327e40caf8af06a5ee641c2715426b68f8842
Fulya Akan <fulyaakan@outlook.com>
2026-04-29 00:58:20 +0300

    .

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,92 +9,113 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var firstValue: Double = 0 //işlemden önceki sayı
-    var secondValue: Double = 0 //işlemden sonraki sayı
-    var currentOperation: String = ""
-    var isTypingNumber: Bool = false //kullanıcı ikinci sayıyı yazmaya basladı
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    enum Operation: Int {
+        case add = 10
+        case subtract = 11
+        case multiply = 12
+        case divide = 13
+        case none = 0 //bir islem yapılmadıysa
     }
-
+    
+    var runningNumber: String = "" //ekranda o an yazan sayı
+    var leftValue: String = ""
+    var rightValue: String = ""
+    var result: String = ""
+    var currentOperation: Operation = .none
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func numberPressed(_ sender: UIButton) {
-        //take the number on button, sender yazdıgın butonu temsil eder
-        guard let numberText = sender.currentTitle else { return }
-        
-        if isTypingNumber == true {
-            //eğer islem butonuna basıldıktan sonra ılk rakamsa, ekran,
-            resultLabel.text = numberText
-            isTypingNumber = false
-        } else {
-            //add numver to label yazıyı labele ekle
-            if resultLabel.text == "0" {
-                resultLabel.text = numberText
-            } else {
-                // + --> string connection
-                resultLabel.text = resultLabel.text! + numberText
-            }
-        }
+        runningNumber += "\(sender.tag)"
+        //resultLabel.text = runningNumber
+        updateDisplay()
     }
     
     @IBAction func actionPressed(_ sender: UIButton) {
-        guard let operationSign = sender.currentTitle else { return }
+        currentOperation = Operation(rawValue: sender.tag) ?? .none
+        leftValue = runningNumber //ekrana yazılmıs sayıyı soldaki değer olarak kaydet
+        runningNumber = ""
         
-        if operationSign == "AC" {
-            firstValue = 0
-            secondValue = 0
-            currentOperation = ""
-            resultLabel.text = "0"
-            isTypingNumber = false
-        } else if operationSign == "=" {
-            calculateResult()
-            isTypingNumber = true //sonuctan sonra direk sayıya basılırsa ekran
-        } else {
-            currentOperation = operationSign
-            firstValue = Double(resultLabel.text!)! // ?? 0
-            isTypingNumber = true
-        }
+        //updateDisplay()
+        //resultLabel.text = "" //operation verdiğimde text temizlensin
     }
     
-    func calculateResult() {
-        //ekranda görülen son sayıyı alıyoruz
-        secondValue = Double(resultLabel.text!) ?? 0
-        
-        var result: Double = 0
+    @IBAction func equalPressed(_ sender: UIButton) {
+        rightValue = runningNumber
+        let firstNumber = Double(leftValue) ?? 0
+        let secondNumber = Double(rightValue) ?? 0
         
         switch currentOperation {
-        case "+":
-            result = firstValue + secondValue
-            resultLabel.text = String(result)
-        case "-":
-            result = firstValue - secondValue
-            resultLabel.text = String(result)
-        case "*":
-            result = firstValue * secondValue
-            resultLabel.text = String(result)
-        case "÷":
-            if secondValue != 0 {
-                result = firstValue / secondValue
-            } else {
-                resultLabel.text = "Error"
-                return
-            }
-        default:
-            return
+        case .add:
+            result = "\(firstNumber + secondNumber)"
+        case .subtract:
+            result = "\(firstNumber - secondNumber)"
+        case .multiply:
+            result = "\(firstNumber * secondNumber)"
+        case .divide:
+            result = "\(firstNumber / secondNumber)"
+        case .none:
+            break
         }
+        runningNumber = result //sonucu saklıyorum
+        //resultLabel.text = result
+        updateDisplay()
         
-        //sonucu ekranda goster fkaat ram sayı küsuratını gizle
-        if result.truncatingRemainder(dividingBy: 1) == 0 {
-            resultLabel.text = String(format: "%.0f", result)
-        } else {
-            resultLabel.text = String(result)
-        }
-        
-        firstValue = result
+      
+        updateDisplay()
+        //resultLabel.text = result
+        //leftvalue = result
         
     }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        //arka plandaki verileri sıfırlar
+        runningNumber  = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .none
+        
+        updateDisplay()
+        //resultLabel.text = "0" // kullanıcıya her seyın sılındıgını gosterır. bir görsel nesne oldugu ıcın.text diyoruz fakat yukarıdakiler gorsel nesne değil string direk "" denir
+    }
+    
+    @IBAction func changeSignPressed(_ sender: UIButton) {
+        let signedNumber = (Double(runningNumber) ??  0) * -1
+        runningNumber = "\(signedNumber)" // hafızaya kaydet - Stringe cevirmemize gerek kalmadı \ aynı işi yapar
+        //resultLabel.text = runningNumber
+        updateDisplay()
+    }
+    
+    @IBAction func percentPressed(_ sender: UIButton) {
+        let signedNumber = (Double(runningNumber) ??  0) / 100
+        runningNumber = "\(signedNumber)"
+        //resultLabel.text = runningNumber
+        updateDisplay()
+    }
+    
+    func updateDisplay() {
+        if runningNumber.hasSuffix(".0") {
+            runningNumber = String(runningNumber.dropLast(2))
+        }
+        
+        let formattedText = runningNumber.replacingOccurrences(of: ".", with: ",")
+        resultLabel.text = formattedText.isEmpty ? "0" : formattedText
+    }
+    
+    @IBAction func dotPressed(_ sender: UIButton) {
+        if !runningNumber.contains(".") {
+            runningNumber = runningNumber.isEmpty ? "0." : runningNumber + "."
+            updateDisplay()
+            //if runningNumber.isEmpty {
+            //runningNumber += "0."
+            //} else {
+            //    runningNumber += "."
+            //}
+            //resultLabel.text = runningNumber.replacingOccurrences(of: ".", with: ",")
+        }
+        
+    }
+    
 }
 
