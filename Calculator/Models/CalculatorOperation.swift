0ab4c6d73a6393ea075fb073ed9b52b08ce78e8f
Fulya Akan <fulyaakan@outlook.com>
2026-05-07 23:24:03 +0300

    .

---
+++ Calculator/Models/CalculatorOperation.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorOperation.swift
+//  Calculator
+//
+//  Created by fulya akan on 7.05.2026.
+//
+enum Operation: Int {
+    case add = 10
+    case subtract
+    case multiply
+    case divide
+    case modulus = 14
+}
--- Calculator/Screens/Home/HomeViewController.swift
+++ Calculator/Screens/Home/HomeViewController.swift
@@ -10,41 +10,39 @@ import UIKit
 class ViewController: UIViewController {
     //homeviewcontroller yap
     
-    enum Operation: Int {
-        case add = 10
-        case subtract
-        case multiply
-        case divide
-        case modulus = 14
-    }
-    
-    var runningNumber: String = ""
-    var leftValue: String = ""
-    var rightValue: String = ""
-    var result: String = ""
-    var currentOperation: Operation? = nil
-    
     @IBOutlet weak var resultLabel: UILabel!
     
+    let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 2
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    
+    private var currentInput: String = ""
+    private var firstValue: String = ""
+    private var secondValue: String = ""
+    private var result: String = ""
+    private var currentOperation: Operation? = nil
+    
+    
     @IBAction func numberPressed(_ sender: UIButton) {
-        runningNumber += "\(sender.tag)"
+        currentInput += "\(sender.tag)"
         updateDisplay()
     }
     
     @IBAction func actionPressed(_ sender: UIButton) {
-        if runningNumber == "" {
-            return
-        }
-        leftValue = runningNumber
-        runningNumber = ""
+        if currentInput.isEmpty { return }
+        firstValue = currentInput
+        currentInput = ""
         currentOperation = Operation(rawValue: sender.tag)
-        
     }
     
     @IBAction func equalPressed(_ sender: UIButton) {
-        rightValue = runningNumber
-        let firstNumber = Double(leftValue) ?? 0
-        let secondNumber = Double(rightValue) ?? 0
+        secondValue = currentInput
+        let firstNumber = Double(firstValue) ?? 0
+        let secondNumber = Double(secondValue) ?? 0
         var rawResult: Double = 0
         
         //code reviewda dizi mantıgına bakılacak
@@ -58,80 +56,59 @@ class ViewController: UIViewController {
         case .modulus:
             if secondNumber != 0 {
                 rawResult = firstNumber.truncatingRemainder(dividingBy: secondNumber)
-                    result = "\(rawResult)"
-                //???
+                result = "\(rawResult)"
             }
         case .multiply:
             rawResult = firstNumber * secondNumber
-            if String(rawResult).count > 8 {
-                result = String(format: "%2f", rawResult)
-            } else {
-                result = "\(rawResult)"
-            }
+            result = String(format: "%2f", rawResult)
         case .divide:
             if secondNumber == 0 {
                 result = "undefined"
             } else {
                 rawResult = firstNumber / secondNumber
-                if rawResult.truncatingRemainder(dividingBy: 1) == 0 {
-                    result = "\(Int(rawResult))"
-                } else {
-                    result = String(format: "%.2f", rawResult)
-                    
-                    while result.hasSuffix("0"){
-                        result.removeLast()
-                    }
-                    if result.hasSuffix("."){
-                        result.removeLast()
-                    }
-                }
+                result = formatter.string(from: NSNumber(floatLiteral: rawResult)) ?? "\(rawResult)"
             }
         case nil:
             break
         }
         
-        runningNumber = result
+        currentInput = result
         updateDisplay()
-        
-        updateDisplay()
-        
     }
     
     @IBAction func clearPressed(_ sender: UIButton) {
-        runningNumber  = ""
-        leftValue = ""
-        rightValue = ""
+        clear()
+    }
+    
+    @IBAction func changeSignPressed(_ sender: UIButton) {
+        let signedNumber = (Double(currentInput) ??  0) * -1
+        currentInput = "\(signedNumber)"
+        updateDisplay()
+    }
+    
+    @IBAction func dotPressed(_ sender: UIButton) {
+        guard !currentInput.contains(".") else { return }
+        currentInput = currentInput.isEmpty ? "0." : currentInput + "."
+        updateDisplay()
+    }
+    
+    private func updateDisplay() {
+        if currentInput.hasSuffix(".0") {
+            currentInput = String(currentInput.dropLast(2))
+        }
+        let formattedText = currentInput.replacingOccurrences(of: ".", with: ",")
+        resultLabel.text = formattedText.isEmpty ? "0" : formattedText
+    }
+    
+    private func clear() {
+        currentInput  = ""
+        firstValue = ""
+        secondValue = ""
         result = ""
         currentOperation = nil
         
         updateDisplay()
     }
     
-    @IBAction func changeSignPressed(_ sender: UIButton) {
-        let signedNumber = (Double(runningNumber) ??  0) * -1
-        runningNumber = "\(signedNumber)"
-        updateDisplay()
-    }
-    
-   // @IBAction func percentPressed(_ sender: UIButton) {
-     //   let signedNumber = (Double(runningNumber) ??  0) / 100
-       // runningNumber = "\(signedNumber)"
-        //updateDisplay()
-  //  }
-    
-    func updateDisplay() {
-        if runningNumber.hasSuffix(".0") {
-            runningNumber = String(runningNumber.dropLast(2))
-        }
-        let formattedText = runningNumber.replacingOccurrences(of: ".", with: ",")
-        resultLabel.text = formattedText.isEmpty ? "0" : formattedText
-    }
-    
-    @IBAction func dotPressed(_ sender: UIButton) {
-        if !runningNumber.contains(".") {
-            runningNumber = runningNumber.isEmpty ? "0." : runningNumber + "."
-            updateDisplay()
-        }
-    }
 }
 
